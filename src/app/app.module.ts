933c857b40869cd9a6100661874ffedf8befb45b
Arthur Alves <arthuralves91@hotmail.com>
2019-10-26 15:08:01 -0300

    Title bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { TitleBarComponent } from './shared/components/title-bar/title-bar.component';
 import { SharedComponents } from './shared/components/shared-components.module';
 
 const routes: Routes = [{
